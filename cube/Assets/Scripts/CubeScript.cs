f9e8c9de3749415563c7f3eadcc3fac9c344af7e
Long To <long.toquoc@gmail.com>
2020-05-10 18:46:41 +0700

    - update scene

--- cube/Assets/Scripts/CubeScript.cs
+++ cube/Assets/Scripts/CubeScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeBehaviourScript : MonoBehaviour
+public class CubeScript : MonoBehaviour
 {
     // Start is called before the first frame update
     private Transform mTransform;
